3b4225993d0be1d60d068d7894fd7a381406d156
Danilo Chaves <daniltojs@yahoo.com.br>
2025-03-11 01:23:05 -0300

    update

--- iac/provider.tf
+++ iac/provider.tf
@@ -9,8 +9,15 @@ terraform {
       version = "~> 4.0"
     }
   }
-}
 
+  backend "s3" {
+      bucket         = "terraform-fullstate"
+      key            = "devops.tfstate"
+      region         = "us-east-1"
+      encrypt        = "true"
+      role_arn        = "arn:aws:iam::361769602634:role/terraform-tfstate-s3"
+  }
+}
 
 provider "aws" {
   region  = var.aws_region
